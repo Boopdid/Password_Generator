e2f3496cb5a219f814155f212944f0b7f5385453
Peter Vrutneski <pvrutneski@gmail.com>
2022-03-09 23:05:29 -0700

    created global variables and the prompt and confirms in generatePassword function

--- script.js
+++ script.js
@@ -1,5 +1,91 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var specialCharacters = [
+  "!",
+  '"',
+  "#",
+  "$",
+  "%",
+  "&",
+  "'",
+  "(",
+  ")",
+  "*",
+  "+",
+  ",",
+  "-",
+  ".",
+  "/",
+  ":",
+  ";",
+  "<",
+  "=",
+  ">",
+  "?",
+  "@",
+  "[",
+  "\\",
+  "]",
+  "^",
+  "_",
+  "`",
+  "{",
+  "|",
+  "}",
+  "~",
+];
+var alphabet = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
+var numbers = ["0", "1", "2", "3,", "4", "5", "6", "7", "8", "9"];
+
+function generatePassword() {
+  var passwordLength = prompt("How long do you want your password?");
+  console.log(passwordLength);
+  if (passwordLength >= 8 && passwordLength <= 128) {
+    console.log("Accepted input criteria");
+  } else {
+    console.log("Unaccepted input criteria");
+  }
+  var lowerCase = confirm("Would you like to include lowercase letters?");
+  console.log(lowerCase);
+
+  var upperCase = confirm("Would you like to include uppercase letters?");
+  console.log(upperCase);
+
+  var numbers = confirm("Would you like to include numbers?");
+  console.log(numbers);
+
+  var specialChara = confirm("Would you like to include special characters?");
+  console.log(specialChara);
+
+  return "Temporary password!";
+}
 
 // Write password to the #password input
 function writePassword() {
@@ -7,7 +93,6 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
